6645945c5a9acfb178455fbac3e0c957370a2938
husvik <husvik@live.no>
2017-03-22 12:06:39 +0100

    henter siste

---
+++ TesteNavnVindu/src/NavneVindu.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+class NavneVindu extends JFrame {
+    private JTextField navnefelt = new JTextField(20);
+    private JLabel hilsen = new JLabel("Her kommer det en hilsen");
+
+    public NavneVindu (String tittel){
+        setTitle(tittel);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setLayout(new FlowLayout());
+
+        JLabel ledetekst = new JLabel("Skriv navnet ditt her");
+        add(ledetekst);
+        add(navnefelt);
+
+        JButton knapp = new JButton("Trykk her");
+        add(knapp);
+        KnappeLytter knappelytteren = new KnappeLytter();
+        knapp.addActionListener(knappelytteren);
+
+        add(hilsen);
+        pack();
+    }
+
+    private class KnappeLytter implements ActionListener{
+
+        public void actionPerformed(ActionEvent hendelse){
+
+            String navn = navnefelt.getText();
+            hilsen.setText("Hei på deg, " + navn + "!");
+
+        }
+    }
+
+
+}
---
+++ TesteNavnVindu/src/TesteNavnVindu.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class TesteNavnVindu {
+    public static void main(String[] args) {
+        NavneVindu etVindu = new NavneVindu("test");
+        etVindu.setVisible(true);
+    }
+}
