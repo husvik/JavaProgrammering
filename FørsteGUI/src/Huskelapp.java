2a63c8caaf10f3faf9dee933bfb97857628b9371
husvik <husvik@live.no>
2017-03-28 12:21:35 +0200

    endret småting

--- FørsteGUI/src/Huskelapp.java
+++ FørsteGUI/src/Huskelapp.java
@@ -1,18 +1,28 @@
 import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by husvi on 28.03.2017.
  */
 public class Huskelapp extends JFrame{
+
     public Huskelapp(){
+
         setTitle("Huskelapp");
-        add(new JTextArea());
-        setSize(800,800);
-        setVisible(true);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JTextArea lol = new JTextArea();
+        add(lol);
+        lol.setBackground(Color.yellow);//legger til tekstområde
+        setSize(800,800);//Setter dimensjoner
+        setVisible(true);//Betyr at dette blir synelig når det blir opprettet
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//Hva skal skje når jeg lukker vinduet?
+
+
     }
 
     public static void main(String[] args) {
         Huskelapp a = new Huskelapp();
+
+
     }
 
 }
